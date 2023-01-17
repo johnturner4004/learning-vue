101e68280fdc4c632b41f43b7a06ff9d179c5d10
John Turner <johnturner4004@gmail.com>
2023-01-17 14:30:05 -0600

    init commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+
+  <div id="app">
+    <button @click="count++">Count: {{ count }}</button>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        count: 0
+      }
+    }
+  }
+</script>
